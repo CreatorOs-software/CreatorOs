9ada674c87c810bbb70b78f44c4bdf76594e39d5
Samuel Reinholz <samuelr.kontakt@gmail.com>
2026-07-19 15:49:57 +0200

    Border

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -58,7 +58,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b border-border/40 transition-colors hover:bg-primary/20 data-[state=selected]:bg-muted",
+      "border-b border-border/70 transition-colors hover:bg-primary/20 data-[state=selected]:bg-muted",
       className,
     )}
     {...props}
